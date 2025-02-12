6597bf93385a06950d5fea0f888fa185db3515a0
quantumpat <patrick.carroll715@gmail.com>
2025-02-12 14:17:25 -0700

    Added Epoch One!

---
+++ Epoch One/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using GameEngine.GameObjects;
+
+public class Program
+{
+	public static void Main(string[] args)
+	{
+
+		var engine = new GameEngine.Core.Engine();
+		engine.Config.AutoApplyChanges = true;
+		engine.Run();
+
+		Sprite sprite = new Sprite(engine.GameObjects, "cosmos-bg", 0, 0);
+
+	}
+}
